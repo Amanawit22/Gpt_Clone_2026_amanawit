23bbf7c1175caf2b6ec2f7d400ee303924fb2c84
Amanawit22 <amanawit.behailu@aastustudent.edu.et>
2026-06-02 00:52:11 +0300

    update backend URL and CORS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,8 @@ import MessageList from "./components/MessageList/MessageList";
 import ChatInput from "./components/ChatInput/ChatInput";
 import "./App.css";
 
-const API_BASE_URL = "http://localhost:3888/api";
+const API_BASE_URL =
+  "https://gptclone2026amanawit-production.up.railway.app/api";
 
 function App() {
   const [conversations, setConversations] = useState([]);
